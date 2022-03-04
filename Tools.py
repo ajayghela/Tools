0760d0aadadc5eef950222ddc25f1c696ce7eec8
ajayghela <100951275+ajayghela@users.noreply.github.com>
2022-03-04 15:32:16 +0000

    Add files via upload

---
+++ Tools.py
@@ -0,0 +1,41 @@
+
+.endswith()
+.rstrip()
+.replace(,,)
+.join() #concate string 
+.append() #add somthing to a list or tuple
+.get() #concate into a dictionary
+.split()
+.items() #unpacks a dictionary..  method returns a view object. The view object contains the key-value pairs of the dictionary, as tuples in a list.
+.lower()
+.upper()
+.count()
+.isdigit()
+.translate(string.maketrans(,)) # must be one to one
+.format() #great for returning a string from a dictionary or a string with a particular format
+.sort()
+
+len()
+type()
+range(start, stop, interval) #
+min()
+max()
+sum() #must be an integer
+map(,) #list(map(,)) inside map there are functions: remove_punctuation, str.strip,  str.capitalize(), str.lower(), str.swapcase(), str.title(), and str.upper(). pow()
+reversed( # or using string slice [::-1])
+set() 
+zip() # two lists into dict
+sorted() #automatically sorted by the function lowest to highest although can be changed. i.e reverse = True. 
+enumerate()
+iter() #makes a list iterable
+next() #calls the next element in an iterable list
+
+%s #lets you add a value into a Python string
+{} #indicate placeholders. They do not need any numbers but are simply replaced/formatted in the order the arguments in names are given to format
+
+while
+for 
+if
+elif
+
+import string
